09c883bb6572d385490fda72df73faccdeca3e2d
nrizh31 <naufalrizhaubi@student.uir.ac.id>
2024-06-20 00:35:11 +0700

    commit uas 8

--- src/router/indexRouter.js
+++ src/router/indexRouter.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Todos from './components/Todos.vue'; 
-import Post from './components/Post.vue';
-import Albums from './components/Albums.vue';
+import Todos from '../components/Todos.vue'; 
+import Post from '../components/Post.vue';
+import Albums from '../components/Albums.vue';
 
 const routes = [
-  { path: '/todos', component: Todos }, 
+  { path: '/todos/', component: Todos }, 
   { path: '/post', component: Post }, 
   { path: '/albums', component: Albums } 
 ];
